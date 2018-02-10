55b3c6091af3b313a1dc4e64ad0a65eadb3028f9
arrogantesNick <31374269+arrogantesNick@users.noreply.github.com>
2018-02-10 14:38:39 +0100

    Add files via upload
    Upload v1.0

--- src/main/java/de/nick/detector/events/RenderEvent.java
+++ src/main/java/de/nick/detector/events/RenderEvent.java
@@ -30,38 +30,47 @@ public class RenderEvent {
     private ArrayList<Entity> epearl_event = new ArrayList<Entity>();
     @SubscribeEvent
     public void onRender(RenderGameOverlayEvent e) {
-        if(DetectorMOD.SERVER_IP == null) {//DetectorMOD.SERVER_IP.contains("gommehd.net")
-            for(EntityPlayer entityPlayer : Minecraft.getMinecraft().theWorld.playerEntities) {
-                //if(entityPlayer != Minecraft.getMinecraft().thePlayer) {
-                if(entityPlayer.getHeldItem() != null) {
-                    if(entityPlayer.getHeldItem().getItem() == Item.getItemById(368)) {
-                        if(entityPlayer.getName() != Minecraft.getMinecraft().thePlayer.getDisplayNameString()) {
-                            if(!epearl.contains(entityPlayer)) {
-                                LabyMod.getInstance().displayMessageInChat("§8§l[§6§lEP§8§l] §c§lDer Spieler §6§l" + entityPlayer.getName() + " §c§lhat eine EP");
-                                epearl.add(entityPlayer);
+        if(DetectorMOD.SERVER_IP != null) {
+            if(DetectorMOD.SERVER_IP.contains("gommehd.net") || DetectorMOD.SERVER_IP.contains("cw-bw.net") || DetectorMOD.SERVER_IP.contains("cw-bw.de") ||DetectorMOD.SERVER_IP.contains("frostic.net")) {
+                for(EntityPlayer entityPlayer : Minecraft.getMinecraft().theWorld.playerEntities) {
+                    //if(entityPlayer != Minecraft.getMinecraft().thePlayer) {
+                    if(entityPlayer.getHeldItem() != null) {
+                        if(entityPlayer.getHeldItem().getItem() == Item.getItemById(368)) {
+                            if(entityPlayer.getName() != Minecraft.getMinecraft().thePlayer.getDisplayNameString()) {
+                                if(!epearl.contains(entityPlayer)) {
+                                    LabyMod.getInstance().displayMessageInChat("§8§l[§6§lEP§8§l] §c§lDer Spieler §6§l" + entityPlayer.getName() + " §c§lhat eine EP");
+                                    epearl.add(entityPlayer);
+                                }
+                            }else{
+                                if(!epearl.contains(entityPlayer)) {
+                                    LabyMod.getInstance().displayMessageInChat(MessageMethod.sendMessage("EP", "Du hast deine EP gezeigt!"));
+                                    epearl.add(entityPlayer);
+                                }
+
+                            }
+
+                        }else if(entityPlayer.getHeldItem().getItem() == Item.getItemById(369)){
+                            if(entityPlayer.getName() != Minecraft.getMinecraft().thePlayer.getDisplayNameString()) {
+                                if(!rp.contains(entityPlayer)) {
+                                    LabyMod.getInstance().displayMessageInChat(MessageMethod.sendMessage("RP", "Der Spieler §6§l" + entityPlayer.getName() + " §c§lhat eine RP!"));
+                                    rp.add(entityPlayer);
+                                }
+                            }
+
+
+                        }else if(entityPlayer.getHeldItem().getItem() == Item.getItemById(46)) {
+                            if(entityPlayer.getName() != Minecraft.getMinecraft().thePlayer.getDisplayNameString()) {
+                                if(!rp.contains(entityPlayer)) {
+                                    LabyMod.getInstance().displayMessageInChat(MessageMethod.sendMessage("TNT", "Der Spieler §6§l" + entityPlayer.getName() + " §c§lhat TNT!"));
+                                    rp.add(entityPlayer);
+                                }
                             }
                         }else{
-                            if(!epearl.contains(entityPlayer)) {
-                                LabyMod.getInstance().displayMessageInChat(MessageMethod.sendMessage("EP", "Du hast deine EP gezeigt!"));
-                                epearl.add(entityPlayer);
+                            if(epearl.contains(entityPlayer)) {
+                                epearl.remove(entityPlayer);
                             }
-
-                        }
-
-                    }else if(entityPlayer.getHeldItem().getItem() == Item.getItemById(369)){
-                        if(entityPlayer.getName() != Minecraft.getMinecraft().thePlayer.getDisplayNameString()) {
-                            if(!rp.contains(entityPlayer)) {
-                                LabyMod.getInstance().displayMessageInChat(MessageMethod.sendMessage("RP", "Der Spieler §6§l" + entityPlayer.getName() + " §c§lhat eine RP!"));
-                                rp.add(entityPlayer);
-                            }
-                        }
-
-
-                    }else if(entityPlayer.getHeldItem().getItem() == Item.getItemById(46)) {
-                        if(entityPlayer.getName() != Minecraft.getMinecraft().thePlayer.getDisplayNameString()) {
-                            if(!rp.contains(entityPlayer)) {
-                                LabyMod.getInstance().displayMessageInChat(MessageMethod.sendMessage("TNT", "Der Spieler §6§l" + entityPlayer.getName() + " §c§lhat TNT!"));
-                                rp.add(entityPlayer);
+                            if(rp.contains(entityPlayer)) {
+                                rp.remove(entityPlayer);
                             }
                         }
                     }else{
@@ -72,31 +81,25 @@ public class RenderEvent {
                             rp.remove(entityPlayer);
                         }
                     }
-                }else{
-                    if(epearl.contains(entityPlayer)) {
-                        epearl.remove(entityPlayer);
-                    }
-                    if(rp.contains(entityPlayer)) {
-                        rp.remove(entityPlayer);
-                    }
-                }
 
-                //}
-            }
-            for(Entity entityPlayer : Minecraft.getMinecraft().theWorld.loadedEntityList) {
-                if(entityPlayer.getName() != Minecraft.getMinecraft().thePlayer.getDisplayNameString()) {
-                    if(entityPlayer instanceof EntityEnderPearl) {
-                        if(!epearl_event.contains(entityPlayer)) {
-                            LabyMod.getInstance().displayMessageInChat("§8§l[§6§lEP§8§l] §c§lEine §6§lEP §c§lwurde geworfen!");
-                            epearl_event.add(entityPlayer);
+                    //}
+                }
+                for(Entity entityPlayer : Minecraft.getMinecraft().theWorld.loadedEntityList) {
+                    if(entityPlayer.getName() != Minecraft.getMinecraft().thePlayer.getDisplayNameString()) {
+                        if(entityPlayer instanceof EntityEnderPearl) {
+                            if(!epearl_event.contains(entityPlayer)) {
+                                LabyMod.getInstance().displayMessageInChat("§8§l[§6§lEP§8§l] §c§lEine §6§lEP §c§lwurde geworfen!");
+                                epearl_event.add(entityPlayer);
+                            }
+
+                        }else{
+                            epearl_event.remove(entityPlayer);
                         }
 
-                    }else{
-                        epearl_event.remove(entityPlayer);
                     }
-
                 }
             }
+
         }
 
 
